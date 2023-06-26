c0fb5576ba39f5694245bdb9d5203a08a0bce6cf
emreurkmez <123367318+emreurkmez@users.noreply.github.com>
2023-06-26 18:58:58 +0300

    Add files via upload
    This project contains a web application developed using the Python programming language and the Django web framework.
    
    The website provides a platform for users to interact with a specific purpose in mind. The project is designed to create a fast and secure web application by leveraging the powerful features of Django.

---
+++ Web Application Created with Python and Django/appMy/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import *
+
+def indexPage(request):
+    content2 = Card.objects.get(id=2)
+    news = Card.objects.filter(new=True)
+    cards=Card.objects.all()
+    context ={
+        "content2":content2,
+        "news4":news[:4],
+        "news2":news[:2],
+        "cards":cards,
+
+    }
+    return render(request,"index.html",context)
+def detailPage(request,idcard):
+    card = Card.objects.get(id=idcard)
+    context ={
+        "card":card,
+        
+    }
+    return render(request,"detail.html",context)
+def categoryPage(request):
+    context ={}
+    return render(request,"category.html",context)
